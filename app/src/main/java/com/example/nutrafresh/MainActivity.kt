ba6038e0d6f905a393ae5591c6d7bd4a8bc4ae7f
anurag-crypto <anuavnshandilya@gmail.com>
2023-08-16 00:52:09 +0530

    Intro Done

--- app/src/main/java/com/example/nutrafresh/MainActivity.kt
+++ app/src/main/java/com/example/nutrafresh/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.nutrafresh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.example.nutrafresh.launcher.IntroSlide
+import com.example.nutrafresh.launcher.IntroSliderAdapter
 
 class MainActivity : AppCompatActivity() {
+    private val introSliderAdapter=IntroSliderAdapter(
+        listOf(
+            IntroSlide(
+                "Freshness",
+                "Your Trusted Freshness Indicator for Fruits and Vegetables",
+                R.drawable.freshness
+            ),
+        IntroSlide(
+                    "Storage",
+            "Providing You with Optimal Storage Conditions Measures",
+            R.drawable.storage
+        ),
+        IntroSlide(
+            "Nutrition",
+        "Your Go-To Source for Accurate and Comprehensive Nutrition Information",
+            R.drawable.nutrition
+        )
+        )
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val introSlideViewPager=findViewById<ViewPager2>(R.id.introSliderViewPager)
+        introSlideViewPager.adapter=introSliderAdapter
     }
 }//oouu
